a4a854467b782e83fd9539420a6a1b6918d1a5b3
Joe LenovoLegionPopOs <thebingitkid@gmail.com>
2025-04-19 20:43:04 +0100

    diss labled viol plots

--- Individual project/Workspace/Scripts/plots.py
+++ Individual project/Workspace/Scripts/plots.py
@@ -1,22 +1,34 @@
 from matplotlib import pyplot as plt
 
-from import_data import get_paths as get_paths
-from import_data import get_jpk_batch_data as get_jpk_batch_data
+def YM_viol_plot (batch_data):
+  data = []
+  lables = []
+  for dataset in batch_data:
+    data.append(batch_data[dataset]["Young's Modulus [Pa]"].astype(float))
+    lables.append(dataset)
 
-relative_paths = {
-  'Control': "Curves/txt-export/HK2 Control/HK2 Control_processed-2025.04.18-16.35.00.tsv",
-  'Treated': "Curves/txt-export/HK2 Diseased (TGF-beta1- 10ng per mL, 48h)/HK2 Diseased (TGF-beta1- 10ng per mL, 48h)_processed-2025.04.18-16.25.25.tsv"
-}
+  viols = plt.violinplot(data, showmeans=True, showmedians=True)
 
-paths = get_paths(relative_paths)
+  viols['bodies'][1].set_facecolor('red')
 
-batch_data = get_jpk_batch_data(paths)
+  plt.xticks([1, 2], lables)
+  plt.ylabel("Young's Modulus [Pa]")
+  plt.title("Young's Modulus Comparison")
+  plt.tight_layout()
+  plt.show()
 
-data = []
-lables = []
-for dataset in batch_data:
-  data.append(batch_data[dataset]["Young's Modulus [Pa]"].astype(float))
-  lables.append(dataset)
+def test ():
+  from import_data import get_paths as get_paths
+  from import_data import get_jpk_batch_data as get_jpk_batch_data
 
-plt.violinplot(data)
+  relative_paths = {
+    'Control': "Curves/txt-export/HK2 Control/HK2 Control_processed-2025.04.18-16.35.00.tsv",
+    'Treated': "Curves/txt-export/HK2 Diseased (TGF-beta1- 10ng per mL, 48h)/HK2 Diseased (TGF-beta1- 10ng per mL, 48h)_processed-2025.04.18-16.25.25.tsv"
+  }
 
+  paths = get_paths(relative_paths)
+  batch_data = get_jpk_batch_data(paths)
+
+  YM_viol_plot(batch_data)
+
+test()
