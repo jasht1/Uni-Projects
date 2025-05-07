17681f8b0be46ab32ad38f2cbc3a8e300fd80548
Joe LenovoLegionPopOs <thebingitkid@gmail.com>
2025-05-07 04:33:09 +0100

    IP Calssifier boundry by nSamples figure

--- Individual project/Workspace/Scripts/classifier_perfomance.py
+++ Individual project/Workspace/Scripts/classifier_perfomance.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.colors import Normalize
 from classifier import get_group_probability
 
 
@@ -27,8 +28,6 @@ def classification_threashold(pos_only=False, cbg=True):
   else:
     plt.axhline(0.5, color="gray", linestyle="--", label="Decision Threshold")
   if cbg:  # colour background by priorprob
-    from matplotlib.colors import Normalize
-
     cmap = plt.get_cmap('RdBu')
     norm = Normalize(vmin=0, vmax=1)
     colors = cmap(norm(mc_healthy_probs))
@@ -50,5 +49,60 @@ def classification_threashold(pos_only=False, cbg=True):
   plt.tight_layout()
   plt.show()
 
-# classification_threashold(pos_only=True)
-classification_threashold(pos_only=False)
+# classification_threashold()
+
+from matplotlib.cm import ScalarMappable
+
+def n_samples_effect(
+    ym_range=(0, 2000),
+    sample_sizes=[1, 2, 5, 10, 25, 50, 100],
+    n_ym_points=300,
+    cmap='RdBu_r',
+    mc_resampling=1000
+  ):
+  from import_data import get_results_batch_data
+  batch_data = get_results_batch_data(path="Experiment")
+  control_data = batch_data["Control"]["Young's Modulus [Pa]"]
+  treated_data = batch_data["Treated"]["Young's Modulus [Pa]"]
+  YM_vals = np.linspace(*ym_range, n_ym_points)
+
+  fig, ax = plt.subplots(figsize=(6, 4))
+
+  norm = Normalize(vmin=0, vmax=1)
+  colormap = plt.get_cmap(cmap)
+
+  for i, n in enumerate(sample_sizes):
+    probs = get_group_probability(
+      YM_vals, control_data, treated_data,
+      p_of_group="Treated", n_samples=n,
+      mc_resampling=mc_resampling
+    )
+
+    color_column = colormap(norm(probs))[:, :3]  # (n_ym_points, 3)
+    color_column_2D = np.tile(color_column[:, np.newaxis, :], (1, 20, 1))  # (height, width, RGB)
+    
+    ax.imshow(
+      color_column_2D,
+      extent=[i, i + 1, ym_range[0], ym_range[1]],
+      aspect='auto', origin='lower'
+    )
+
+  # Axis formatting
+  ax.set_xlim(0,len(sample_sizes))
+  ax.set_ylim(ym_range)
+  ax.set_xticks(np.arange(len(sample_sizes)) + 0.5)
+  ax.set_xticklabels(sample_sizes)
+  ax.set_ylabel("Young's Modulus [Pa]")
+  ax.set_xlabel("Number of Cells Sampled")
+  ax.set_title("Classification Boundary vs Sample Size")
+
+  # Colorbar
+  sm = ScalarMappable(norm=norm, cmap=cmap)
+  sm.set_array([])
+  cbar = plt.colorbar(sm, ax=ax)
+  cbar.set_label("P(Diseased | YM)")
+
+  plt.tight_layout()
+  plt.show()
+
+# n_samples_effect(ym_range=(500,1500), mc_resampling=10000)
